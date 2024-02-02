91d067faa0f2eb622b6920e61c2c7e8ae37bdbc3
mhersimonyan2003 <mher.simonyan.2014@gmail.com>
2024-02-02 18:58:19 +0400

    Changed BrowserRouter to HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { Layout } from '@/sections';
 import { ThemeProvider } from '@/theming';
@@ -9,7 +9,7 @@ import { store } from '@/store';
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={store}>
         <ThemeProvider>
           <LocalizationProvider>
@@ -19,7 +19,7 @@ const App = () => {
           </LocalizationProvider>
         </ThemeProvider>
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
